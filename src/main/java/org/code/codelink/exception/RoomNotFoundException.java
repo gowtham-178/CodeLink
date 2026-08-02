35dd79a260ded837f48f03fbd56e0984f2456fdf
gowtham-178 <gowthamdakinedi@gmail.com>
2026-08-02 10:14:10 +0530

    deleted:    .env.example 	deleted:    .gitattributes 	deleted:    .gitignore 	deleted:    .mvn/wrapper/maven-wrapper.properties 	deleted:    README.md 	deleted:    mvnw 	deleted:    mvnw.cmd 	deleted:    pom.xml 	deleted:    src/main/java/org/code/codelink/CodeLinkApplication.java 	deleted:    src/main/java/org/code/codelink/config/AppConfig.java 	deleted:    src/main/java/org/code/codelink/config/RequestTimingFilter.java 	deleted:    src/main/java/org/code/codelink/config/SecurityConfig.java 	deleted:    src/main/java/org/code/codelink/config/WebSocketConfig.java 	deleted:    src/main/java/org/code/codelink/controller/AuthController.java 	deleted:    src/main/java/org/code/codelink/controller/RoomController.java 	deleted:    src/main/java/org/code/codelink/dto/AuthResponse.java 	deleted:    src/main/java/org/code/codelink/dto/ErrorResponse.java 	deleted:    src/main/java/org/code/codelink/dto/LoginRequest.java 	deleted:    src/main/java/org/code/codelink/dto/RegisterRequest.java 	deleted:    src/main/java/org/code/codelink/dto/RoomResponse.java 	deleted:    src/main/java/org/code/codelink/exception/GlobalExceptionHandler.java 	deleted:    src/main/java/org/code/codelink/exception/RoomNotFoundException.java 	deleted:    src/main/java/org/code/codelink/model/Room.java 	deleted:    src/main/java/org/code/codelink/model/User.java 	deleted:    src/main/java/org/code/codelink/repository/RoomRepository.java 	deleted:    src/main/java/org/code/codelink/repository/UserRepository.java 	deleted:    src/main/java/org/code/codelink/security/JwtAuthFilter.java 	deleted:    src/main/java/org/code/codelink/security/JwtService.java 	deleted:    src/main/java/org/code/codelink/security/UserDetailsServiceImpl.java 	deleted:    src/main/java/org/code/codelink/websocket/RoomWebSocketController.java 	deleted:    src/main/java/org/code/codelink/websocket/WsChannelInterceptor.java 	deleted:    src/main/java/org/code/codelink/websocket/WsMessages.java 	deleted:    src/main/resources/application.properties 	deleted:    src/test/java/org/code/codelink/CodeLinkApplicationTests.java 	deleted:    tools/jackson/databind/SerializationFeature.class

--- src/main/java/org/code/codelink/exception/RoomNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.code.codelink.exception;
-
-public class RoomNotFoundException extends RuntimeException {
-    public RoomNotFoundException(String roomId) {
-        super("Room not found or has expired: " + roomId);
-    }
-}
